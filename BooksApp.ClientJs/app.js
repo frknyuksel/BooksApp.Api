c71a275ca9dfed27b373a1846350aaba6602740e
frknyuksel14 <yukselfrkn14@gmail.com>
2023-08-07 15:18:03 +0300

    Apı Project

---
+++ BooksApp.ClientJs/app.js
@@ -0,0 +1,88 @@
+﻿const btnBooks = document.getElementById("btn-books");
+const btnPublishers = document.getElementById("btn-publishers");
+const resultsDiv = document.getElementById("results");
+
+btnBooks.addEventListener('click', getBooks);
+btnPublishers.addEventListener('click', getPublishers);
+
+async function getBooks() {
+    let response = await fetch("http://localhost:5201/api/books");
+    if (response.ok === true) {
+        let jsonResponse = await response.json();
+        setBooks(jsonResponse.Data);
+    }
+    else {
+        console.log("Hata:", response.statusText);
+    }
+}
+
+async function getPublishers() {
+    let response = await fetch("http://localhost:5201/api/publishers");
+    if (response.ok === true) {
+        let jsonResponse = await response.json();
+        setPublishers(jsonResponse.Data);
+    } else {
+        console.log("Hata:", response.statusText);
+    }
+}
+async function getBookDetails(id) {
+    // let response = await fetch("http://localhost:5201/api/books/"+ id);
+    let response = await fetch(`http://localhost:5201/api/books/${id}`);
+    if (response.ok === true) {
+        let jsonResponse = await response.json();
+        setBookDetails(jsonResponse.Data);
+    }
+}
+
+function setBooks(books) {
+    resultsDiv.innerHTML = "";
+    books.forEach(book => {
+        resultsDiv.innerHTML += `
+            <div>
+                <p class="item" onclick="getBookDetails(${book.Id})">
+                    ${book.Name}
+                </p>
+            </div>
+        `;
+    });
+}
+
+function setPublishers(publishers) {
+    resultsDiv.innerHTML = "";
+    publishers.forEach(publisher => {
+        resultsDiv.innerHTML += `
+        <div>
+            <p>${publisher.Name}</p>
+        </div>
+    `;
+    });
+}
+
+function setBookDetails(book) {
+    resultsDiv.innerHTML = `
+    <div class="row">
+        <div class="col-3">
+            <img src="http://localhost:5201/images/books/${book.ImageUrl}" />
+        </div>
+        <div class="col-9">
+            <div class="row">
+                <div class="col-12">
+                    <span class="text-danger">Kitap:</span>
+                    <span>${book.Name}</span>
+                </div>
+                <div class="col-12">
+                    <span class="text-danger">Yazar:</span>
+                    <span>${book.Author.FirstName} ${book.Author.LastName}</span>
+                </div>
+                <div class="col-12">
+                    <span class="text-danger">Yayınevi:</span>
+                    <span>${book.Publisher.Name}</span>
+                </div>
+                <div class="col-12">
+                    <span class="text-danger">Basım Yılı:</span>
+                    <span>${book.EditionYear}</span>
+                </div>
+            </div>
+        </div>
+    </div>`;
+}
---
+++ BooksApp.Mvc/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using BooksApp.Mvc.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Text.Json;
+
+namespace BooksApp.Mvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+        [HttpGet]
+        public async Task<IActionResult> GetAllBooks()
+        {
+            RootList<BookViewModel> rootList  = new RootList<BookViewModel>();
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.GetAsync("http://localhost:5201/api/books"))
+                {
+                    if (!response.IsSuccessStatusCode)
+                    {
+                        return RedirectToAction("Index");
+                    }
+                    string contentResponse = await response.Content.ReadAsStringAsync();
+                    rootList=JsonSerializer.Deserialize<RootList<BookViewModel>>(contentResponse);
+                }
+            }
+            var books = rootList.Data;
+            return View(books);
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetBookById(int id)
+        {
+            Root<BookViewModel> root = new Root<BookViewModel>();
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.GetAsync($"http://localhost:5201/api/books/{id}"))
+                {
+                    if (!response.IsSuccessStatusCode)
+                    {
+                        return RedirectToAction("GetAllBooks");
+                    }
+                    string contentResponse = await response.Content.ReadAsStringAsync();
+                    root = JsonSerializer.Deserialize<Root<BookViewModel>>(contentResponse);
+                }
+            }
+            var book = root.Data;
+            return View(book);
+        }
+
+        //Publisher'lar için aynı işlemleri yapın.
+    }
+}
